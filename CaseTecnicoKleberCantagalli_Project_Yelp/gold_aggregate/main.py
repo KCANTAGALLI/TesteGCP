e9bb9e008d7722a28007722879710e0ca666688f
KLEBER GUIDO DUTRA CANTAGALLI <47632226+KCANTAGALLI@users.noreply.github.com>
2025-04-04 19:35:53 -0300

    Update main.py

--- CaseTecnicoKleberCantagalli_Project_Yelp/gold_aggregate/main.py
+++ CaseTecnicoKleberCantagalli_Project_Yelp/gold_aggregate/main.py
@@ -1,26 +1,35 @@
 from google.cloud import bigquery
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 def aggregate_yelp_data(event, context):
-    client = bigquery.Client()
-    dataset_id = "yelp_data"
-    table_id = "gold_insights"
+    try:
+        client = bigquery.Client()
+        dataset_id = "yelp_data"
+        table_id = "gold_insights"
 
-    job_config = bigquery.QueryJobConfig(
-        destination=f"{client.project}.{dataset_id}.{table_id}",
-        write_disposition="WRITE_TRUNCATE"
-    )
+        job_config = bigquery.QueryJobConfig(
+            destination=f"{client.project}.{dataset_id}.{table_id}",
+            write_disposition="WRITE_TRUNCATE"
+        )
 
-    sql = f"""
-    SELECT
-      city,
-      categories,
-      COUNT(*) AS total_restaurantes,
-      AVG(rating) AS media_avaliacao,
-      SUM(review_count) AS total_reviews
-    FROM
-      `{client.project}.{dataset_id}.silver_table`
-    GROUP BY city, categories
-    """
+        sql = f"""
+        SELECT
+          city,
+          categories,
+          COUNT(*) AS total_restaurantes,
+          AVG(rating) AS media_avaliacao,
+          SUM(review_count) AS total_reviews
+        FROM
+          `{client.project}.{dataset_id}.silver_table`
+        GROUP BY city, categories
+        """
 
-    query_job = client.query(sql, job_config=job_config)
-    query_job.result()
+        query_job = client.query(sql, job_config=job_config)
+        query_job.result()
+        logging.info("Agregação da camada Gold concluída e salva no BigQuery.")
+
+    except Exception as e:
+        logging.error(f"Erro ao agregar dados na camada Gold: {e}")
+        raise
